92c4d99578a0b4acb305027f9d8ced7d205d8ca3
Yeet <Yeet@LILROOKIE>
2022-06-20 21:20:39 +1200

    Add project files.

---
+++ Project/Models/DB/TruckDetailsDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project.Models.DB
+{
+    public class TruckDetailsDisplay
+    {
+        [Key]
+        public int TruckId { get; set; }
+        public string Model { get; set; } = null!;
+        public string Colour { get; set; } = null!;
+        public decimal DailyRentalPrice { get; set; }
+        public string Size { get; set; } = null!;
+        public string? Status { get; set; } = null!;
+    }
+}
